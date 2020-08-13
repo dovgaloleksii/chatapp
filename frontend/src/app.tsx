98adb0b5b3c0b69407caf5fb1962d4504c6fe981
Oleksii Dovgal <dovgaloleksii@gmail.com>
2020-08-13 20:36:41 +0300

    Add login form

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -2,6 +2,8 @@ import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import { ThemeProvider } from '@material-ui/core/styles';
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import { theme } from './theme';
 import { MainRouter } from './router';
 import './app.scss';
@@ -10,6 +12,17 @@ import { ProvideAuth } from './components/auth';
 export const App: React.FunctionComponent = () => (
   <>
     <CssBaseline />
+    <ToastContainer
+      position="bottom-right"
+      autoClose={5000}
+      hideProgressBar={false}
+      newestOnTop
+      closeOnClick
+      rtl={false}
+      pauseOnFocusLoss
+      draggable
+      pauseOnHover
+    />
     <ThemeProvider theme={theme}>
       <Router>
         <ProvideAuth>
--- frontend/src/pages/login/index.tsx
+++ frontend/src/pages/login/index.tsx
@@ -1,24 +1,81 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import { Button } from '@material-ui/core';
+import { Button, Grid, Paper } from '@material-ui/core';
+import { createStyles, makeStyles, Theme } from '@material-ui/core/styles';
+import Typography from '@material-ui/core/Typography';
+import { toast } from 'react-toastify';
+import { Field, Form, Formik } from 'formik';
+import { TextField } from 'formik-material-ui';
 import { useAuthContext } from '../../components/auth/context';
 import { Page } from '../../components/base-page/page';
+
+const useStyles = makeStyles((theme: Theme) =>
+  createStyles({
+    root: {
+      padding: theme.spacing(2),
+      width: 'unset',
+
+      '&>div, &>button': {
+        margin: theme.spacing(2),
+      },
+      '&>.MuiTypography-root': {
+        flex: 1,
+      },
+    },
+  }),
+);
 
 export const Login: React.FunctionComponent = () => {
   const { login } = useAuthContext();
   const history = useHistory();
+  const classes = useStyles();
 
   return (
     <Page pageTitle="Login">
-      <Button
-        color="primary"
-        onClick={() => {
+      <Formik
+        initialValues={{
+          username: '',
+          password: '',
+        }}
+        onSubmit={(...args) => {
+          window.console.log(args);
+          toast.info('Welcome');
           login();
           history.push('/');
         }}
       >
-        Login
-      </Button>
+        {({ handleReset, handleSubmit }) => (
+          <Form onSubmit={handleSubmit} onReset={handleReset}>
+            <Paper>
+              <Grid container className={classes.root}>
+                <Typography align="center" variant="h5">
+                  Login
+                </Typography>
+                <Field
+                  component={TextField}
+                  name="username"
+                  label="Username"
+                  variant="outlined"
+                  autoComplete="current-password"
+                  fullWidth
+                />
+                <Field
+                  component={TextField}
+                  name="password"
+                  label="Password"
+                  type="password"
+                  autoComplete="current-password"
+                  variant="outlined"
+                  fullWidth
+                />
+                <Button color="primary" variant="contained" type="submit" fullWidth>
+                  Login
+                </Button>
+              </Grid>
+            </Paper>
+          </Form>
+        )}
+      </Formik>
     </Page>
   );
 };
